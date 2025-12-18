09d0b2537e98f5ce4196c2322613ec8649c2a625
hamzakabooro <kaboorohamzakhan32@gmail.com>
2025-12-19 02:59:21 +0500

    update

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-organization.github.io', // Replace with your GitHub Pages URL
+  url: 'https://ai-driven-humanoid-robots-omkauq5mj-kaboorohamzas-projects.vercel.app/', // Replace with your GitHub Pages URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/', // Adjust for your repository name
